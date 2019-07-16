9fe466040a5e08b790e10f6f31f2e9d07c2f3730
alyonaprylypa <alyonka130198@gmail.com>
2019-07-16 21:32:15 +0300

    new model

--- cmd/realtor_service/main.go
+++ cmd/realtor_service/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"Project/controllers"
-	"Project/driver"
-	"database/sql"
+	"Project/pkg/controllers"
+	"Project/pkg/driver"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-var db *sql.DB
-
-
 func main() {
-	db=driver.ConnectDB()
+	db:= driver.ConnectDB()
 	defer db.Close()
 	router := mux.NewRouter()
-	controller:=controllers.Controller{}
+	controller:= controllers.Controller{}
 	router.HandleFunc("/flats", controller.GetFlats(db)).Methods("GET")
 	router.HandleFunc("/flats/{id}", controller.GetFlat(db)).Methods("GET")
 	router.HandleFunc("/login",controller.LoginPageHandler(db)).Methods("GET")
