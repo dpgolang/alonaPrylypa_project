8e0b9b542c4fb94f035d99a1e262162d25675a7d
alyonaprylypa <alyonka130198@gmail.com>
2019-07-17 16:54:13 +0300

    gofmt my code

--- pkg/repos/UserRepository.go
+++ pkg/repos/UserRepository.go
@@ -2,24 +2,24 @@ package repos
 
 import "io/ioutil"
 
-func UserIsValid(uName,pwd string)bool{
-	_uName,_pwd, isValid:="alyonka","190117",false
-	if uName==_uName&&pwd==_pwd{
-		isValid=true
+func UserIsValid(uName, pwd string) bool {
+	_uName, _pwd, isValid := "alyonka", "190117", false
+	if uName == _uName && pwd == _pwd {
+		isValid = true
 	}
 	return isValid
 }
-func IsEmpty(data string)bool{
-	if len(data)<=0{
+func IsEmpty(data string) bool {
+	if len(data) <= 0 {
 		return true
-	}else{
+	} else {
 		return false
 	}
 }
-func LoadFile(fileName string)(string,error){
-	bytes,err:=ioutil.ReadFile(fileName)
-	if err!=nil{
-		return "",err
+func LoadFile(fileName string) (string, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return "", err
 	}
 	return string(bytes), nil
 }
